f3f91cb8f924923caf791988c991e63b1c1c1f1a
manulecam <lecam.manuel@gmail.com>
2022-12-16 00:10:17 -0300

    Segundo commit

---
+++ establece-parmetros-por-defecto-para-tus-funciones.js
@@ -0,0 +1,5 @@
+// Cambia solo el código debajo de esta línea
+const increment = (number = (number+1), value = 1) => number + value;
+
+console.log(increment())
+// Cambia solo el código encima de esta línea
---
+++ utiliza-el-operador-de-propagacin-para-evaluar-los-arreglos-en-el-lugar.js
@@ -0,0 +1,7 @@
+const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
+let arr2;
+
+arr2 = [...arr1];  // Cambia esta línea
+console.log(...arr1)
+
+console.log(arr2);
